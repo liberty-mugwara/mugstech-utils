ebdd74de5dad86c51f13d0f47149a0354e7b9e32
Liberty Mugwara <libertymugwara@live.com>
2022-12-14 21:27:24 +0200

    FEAT: Add more detail to format fns

---
+++ src/format-fns.ts
@@ -0,0 +1,18 @@
+interface IFormatFnReturn {
+  data: string;
+  extension: "JSON";
+  jsonType: "OBJECT" | "ARRAY";
+}
+
+export function formatVariantIdsFromBulkQuery(data: string): IFormatFnReturn {
+  const variant: { id: string; sku: string; product: { id: string } } =
+    JSON.parse(data);
+  return {
+    data: `"${variant.sku}":${JSON.stringify([
+      variant.id,
+      variant.product.id,
+    ])}`,
+    extension: "JSON",
+    jsonType: "OBJECT",
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export function formatVariantIdsFromBulkQuery(data: string) {
-  const variant: { id: string; sku: string; product: { id: string } } =
-    JSON.parse(data);
-  return `"${variant.sku}":${JSON.stringify([variant.id, variant.product.id])}`;
-}
+export * from "./format-fns";
